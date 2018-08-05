b43edf3651e58b62d96dfab8e7fb7579f020a073
nthnmillar <nthnmillar@gmail.com>
2018-08-06 08:10:53 +1000

    started c#

--- Assets/Scripts/MaterialGradientModifier.cs
+++ Assets/Scripts/MaterialGradientModifier.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MaterialGradientModifier : MonoBehaviour {
+[RequireComponent(typeof(Renderer))]
+public class MaterialGradientModifier : MonoBehaviour
+{
+    Renderer _renderer;
 
-	// Use this for initialization
-	void Start () {
-		
+    // public Color myColor;
+    [SerializeField] Gradient gradient;
+
+    float gradientPosition;
+    void SetGradientPosition(float position)
+    {
+        if (position == gradientPosition)
+            return;
+        gradientPosition = position;
+
+        _renderer.material.color = gradient.Evaluate(gradientPosition);		
+    }
+
+    void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        SetGradientPosition(0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        SetGradientPosition(Mathf.Sin ((Time.time) * 0.5f) + 0.5f);
 	}
 }
