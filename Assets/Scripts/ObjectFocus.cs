d27d72429946f390bc84909d5cf70daa5fabc076
nthnmillar <nthnmillar@gmail.com>
2018-08-07 20:59:40 +1000

    started camera code

---
+++ Assets/Scripts/ObjectFocus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocus : MonoBehaviour
+{
+    [SerializeField] Transform referance;
+
+    private float _fadeAmount;
+    public float fadeAmount
+    {
+        get { return _fadeAmount; }
+        set
+        {
+            if (value != _fadeAmount)
+            {
+                _fadeAmount = value;
+            }
+        }
+    }
+
+	void Awake ()
+    {
+        //  referance = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        if (!referance)
+           referance = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
